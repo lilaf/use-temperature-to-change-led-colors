1971361cd4a736045cc85a030456624fa6b58757
lilaf <14340799+lilaf@users.noreply.github.com>
2020-06-25 01:13:09 -0600

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,21 @@
+let TempF = 0
+let TempC = 0
+let strip = neopixel.create(DigitalPin.P2, 30, NeoPixelMode.RGB)
+let HighTemp = 72
+basic.forever(function () {
+    dht11_dht22.queryData(
+    DHTtype.DHT22,
+    DigitalPin.P0,
+    true,
+    true,
+    true
+    )
+    TempC = dht11_dht22.readData(dataType.temperature)
+    TempF = TempC * (9 / 5) + 32
+    if (TempF > 72) {
+        strip.showColor(neopixel.colors(NeoPixelColors.Orange))
+    } else {
+        strip.showColor(neopixel.colors(NeoPixelColors.Blue))
+    }
+    strip.show()
+})
